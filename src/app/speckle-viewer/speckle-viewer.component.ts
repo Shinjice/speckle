810c0ce26331a861b4d4d94346829997150e68fa
Tam Macbook <tdnda84@gmail.com>
2025-02-28 21:50:23 +0100

    viewer example

--- src/app/speckle-viewer/speckle-viewer.component.ts
+++ src/app/speckle-viewer/speckle-viewer.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  Viewer,
+  DefaultViewerParams,
+  SpeckleLoader,
+  UrlHelper,
+} from '@speckle/viewer';
+import { CameraController, SelectionExtension } from '@speckle/viewer';
 
 @Component({
   selector: 'app-speckle-viewer',
@@ -6,6 +13,41 @@ import { Component } from '@angular/core';
   templateUrl: './speckle-viewer.component.html',
   styleUrl: './speckle-viewer.component.css'
 })
-export class SpeckleViewerComponent {
+export class SpeckleViewerComponent implements OnInit {
+  ngOnInit(): void {
+    this.createViewer();
+  }
+
+  @ViewChild('renderer') renderer!: ElementRef;
+
+
+  public async createViewer(){
+    const container = this.renderer as unknown as HTMLElement;
+
+  /** Configure the viewer params */
+  const params = DefaultViewerParams;
+  params.showStats = false;
+  params.verbose = true;
+
+  /** Create Viewer instance */
+  const viewer = new Viewer(container, params);
+  /** Initialise the viewer */
+  await viewer.init();
+
+  /** Add the stock camera controller extension */
+  viewer.createExtension(CameraController);
+  /** Add the selection extension for extra interactivity */
+  viewer.createExtension(SelectionExtension);
+
+  /** Create a loader for the speckle stream */
+  const urls = await UrlHelper.getResourceUrls(
+    'https://app.speckle.systems/projects/24c98619ac/models/38639656b8'
+  );
+  for (const url of urls) {
+    const loader = new SpeckleLoader(viewer.getWorldTree(), url, '');
+    /** Load the speckle data */
+    await viewer.loadObject(loader, true);
+  }
+  }
 
 }
