98c913385ddc0ef0b16eabddf4decebfd6f68620
Tam Desktop <Tam@brandmannen.nl>
2025-03-01 20:43:41 +0100

    improved viewer a bit

--- src/app/speckle-viewer/speckle-viewer.component.ts
+++ src/app/speckle-viewer/speckle-viewer.component.ts
@@ -1,4 +1,10 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import {
   Viewer,
   DefaultViewerParams,
@@ -11,43 +17,41 @@ import { CameraController, SelectionExtension } from '@speckle/viewer';
   selector: 'app-speckle-viewer',
   standalone: false,
   templateUrl: './speckle-viewer.component.html',
-  styleUrl: './speckle-viewer.component.css'
+  styleUrl: './speckle-viewer.component.css',
 })
-export class SpeckleViewerComponent implements OnInit {
-  ngOnInit(): void {
+export class SpeckleViewerComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
     this.createViewer();
   }
 
   @ViewChild('renderer') renderer!: ElementRef;
 
+  public async createViewer() {
+    const container = this.renderer.nativeElement as HTMLElement;
 
-  public async createViewer(){
-    const container = this.renderer as unknown as HTMLElement;
+    /** Configure the viewer params */
+    let { showStats, verbose } = DefaultViewerParams;
+    showStats = false;
+    verbose = true;
 
-  /** Configure the viewer params */
-  const params = DefaultViewerParams;
-  params.showStats = false;
-  params.verbose = true;
+    /** Create Viewer instance */
+    const viewer = new Viewer(container, DefaultViewerParams);
+    /** Initialise the viewer */
+    await viewer.init();
 
-  /** Create Viewer instance */
-  const viewer = new Viewer(container, params);
-  /** Initialise the viewer */
-  await viewer.init();
+    /** Add the stock camera controller extension */
+    viewer.createExtension(CameraController);
+    /** Add the selection extension for extra interactivity */
+    viewer.createExtension(SelectionExtension);
 
-  /** Add the stock camera controller extension */
-  viewer.createExtension(CameraController);
-  /** Add the selection extension for extra interactivity */
-  viewer.createExtension(SelectionExtension);
-
-  /** Create a loader for the speckle stream */
-  const urls = await UrlHelper.getResourceUrls(
-    'https://app.speckle.systems/projects/24c98619ac/models/38639656b8'
-  );
-  for (const url of urls) {
-    const loader = new SpeckleLoader(viewer.getWorldTree(), url, '');
-    /** Load the speckle data */
-    await viewer.loadObject(loader, true);
+    /** Create a loader for the speckle stream */
+    const urls = await UrlHelper.getResourceUrls(
+      'https://app.speckle.systems/projects/24c98619ac/models/38639656b8'
+    );
+    for (const url of urls) {
+      const loader = new SpeckleLoader(viewer.getWorldTree(), url, '');
+      /** Load the speckle data */
+      await viewer.loadObject(loader, true);
+    }
   }
-  }
-
 }
